0f689a944333baa0c5c8da6a36dd3405d82036e2
alex-hyman37 <alex.hyman@comcast.net>
2021-10-19 13:00:10 -0400

    new files

---
+++ js/app.js
@@ -0,0 +1,51 @@
+particlesJS.load('particles-js', 'js/particles.json', function(){
+  console.log('particles.json loaded...');
+});
+
+
+//Hover card
+let cards = document.querySelectorAll(".card");
+
+
+cards.forEach(card => {
+  let moreInfo = card.querySelector(".more-info");
+
+  card.addEventListener('click', (event) => {
+    if(event.target.classList == "more-info") {
+      card.classList.toggle('is-flipped');
+    }
+  });
+
+  window.addEventListener('click', event => {
+    if (!event.target.classList.contains('more-info')) {
+        card.classList.remove('is-flipped');
+    }
+  });
+});
+
+// Smooth scroll
+const scroll = new SmoothScroll('.contact-button a[href*="#"]', {
+  speed: 800
+});
+
+
+
+
+// window.addEventListener('click', event => {
+//   if (!event.target.classList.contains('more-info')) {
+//     cards.forEach(card => {
+//       card.classList.remove('is-flipped');
+//     });
+//   }
+// });
+
+
+
+
+
+// let moreInfo = document.querySelector(".more-info");
+// let card = document.querySelector(".card");
+
+// moreInfo.addEventListener('click', (e) => {
+//   card.classList.toggle('is-flipped');
+// });
